d6471dbf4bf594e2626ddc31b2348624e12a7e61
Giovanny Gonzalez <henrygiovanny.gonzalezwaltero@mercadolibre.com.co>
2021-07-02 20:28:33 -0500

    intro javascript

---
+++ html_css/js/intro.js
@@ -0,0 +1,14 @@
+var mensaje = 'Hola mundo';
+let mensaje2 = ' aprendices de multimedia!';
+console.log(mensaje);
+console.log(mensaje + mensaje2);
+
+let num1 = 10;
+let num2 = 20;
+num1 = 15.4;
+console.log(num1, num2);
+console.log(num1 + num2);
+
+const esVerdadero = true;
+const esFalso = false;
+console.log(esVerdadero, esFalso);
