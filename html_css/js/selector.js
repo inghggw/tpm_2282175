6e4d548ae688698664160b30d7a4080971f3d598
Giovanny Gonzalez <henrygiovanny.gonzalezwaltero@mercadolibre.com.co>
2021-07-28 19:37:43 -0500

    condicionales y select

--- html_css/js/selector.js
+++ html_css/js/selector.js
@@ -4,5 +4,35 @@ let btn = document.getElementById("btn-ok")
     let msg = document.getElementById("input-msg").value;
     console.log(msg);
 
+    let select = document.getElementById("sel-brand").value;
+    console.log(select);
+
     document.getElementById('msg').innerHTML = msg;
   });
+
+let num = 0;
+
+switch (num) {
+  case 0:
+    console.log('Es cero');
+    break;
+  case 1:
+    console.log('Es uno');
+    break;
+  case 10:
+    console.log('Es diez');
+    break;
+  default:
+    console.log('ningún caso');
+    break;
+}
+
+if (num == 1) {
+  alert('ES UNO');
+} else if (num == 10) {
+  alert('ES DIEZ');
+} else if (num == 20) {
+  alert('ES VEINTE');
+} else {
+  alert('NINGUN CASO');
+}
